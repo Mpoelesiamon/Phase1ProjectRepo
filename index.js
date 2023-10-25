21cf31218def007e2e9f6f0d77cb97cd596c4178
Mpoelesiamon <lesiamon.mpoe@student.moringaschool.com>
2023-10-25 22:04:01 +0300

    third commit

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+const qrDiv= document.querySelector("#qrDiv")
 function generateQRCode() {
-    var phoneNumber = document.getElementById("phone-input").value;
-    var qrcode = new QRCode("qrcode", {
-      text: "tel:" + phoneNumber,
-      width: 300,
-      height: 300,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.H
-    });
-    
-    // Show download link
-    document.getElementById("download-link").style.display = "block";
+    let phoneNumber = document.getElementById("phone-input").value;
+    let numberPattern = /^\d{10}$|^\d{13}$/;
+
+    // const integerValue = parseInt(phoneNumber, 10);
+
+    if( numberPattern.test(phoneNumber)){
+        var qrcode = new QRCode("qrcode", {
+        text: "tel:" + phoneNumber,
+        width: 300,
+        height: 300,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+        });
+        
+        // Show download link
+        document.getElementById("download-link").style.display = "block";
+    }
+    else{
+        alert("Enter a valid Phone-number")
+    }
+  }
+
+  function refreshPage() {
+    location.reload(); // Reloads the current page
   }
